4d2ec24a139c7715d5ba15a63ba9a72610471486
Timofei Zhakov <tima@chemodax.net>
2024-03-20 17:52:54 +0000

    Fix fetch script: use some git magic...
    git-svn-id: https://svn.rinrab.com/rinrab/SvnToGitMirror/trunk@494 0083e6b4-6090-234a-836e-88fcc467669c

--- Fetch-SvnToGitMirror.ps1
+++ Fetch-SvnToGitMirror.ps1
@@ -6,6 +6,13 @@ param (
 Set-Location $Path
 
 git svn fetch
+git svn rebase
+git fetch --tags $GitRepositoryUrl
 
-git checkout origin/trunk
-git push $GitRepositoryUrl origin/trunk --force
+$branches = $(git branch -r --list 'origin/*').Trim()
+
+foreach ($svnBranch in $branches) {
+    $gitBranch = $svnBranch.TrimStart("origin/")
+    git branch -f $gitBranch $svnBranch
+    git push --tags $GitRepositoryUrl $gitBranch
+}
